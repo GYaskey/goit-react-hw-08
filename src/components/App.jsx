e061975656374dee9e35c494b36243c18dffdf39
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-08-09 18:16:35 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,11 @@
-import { Alert } from './Alert/Alert';
-import Product from './Product/Product';
+import Profile from './Profile/Profile';
+import userData from '../assets/userData.json';
 
 export default function App() {
   return (
-    <div>
-      <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error" outlined>
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success" elevated>
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning" outlined elevated>
-        Please update your profile contact information
-      </Alert>
-    </div>
+    <>
+      <Profile {...userData} />
+      {/* <FriendList /> */}
+    </>
   );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  console.log(stats);
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt={username} />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      {/* <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul> */}
+    </div>
+  );
+};
+
+export default Profile;
