1a5da8b39c5965d6f774cb53e5d61025da42e1ae
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-08-22 00:35:21 +0300

    persist

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,30 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import contactList from '../assets/contacts.json';
 
-const initialState = contactList;
+const initialState = {
+  contacts: contactList,
+  search: '',
+};
 
-[
-  {
-    id: 'id-1',
-    name: 'Rosie Simpson',
-    number: '459-12-56',
-  },
-  {
-    id: 'id-2',
-    name: 'Hermione Kline',
-    number: '443-89-12',
-  },
-  {
-    id: 'id-3',
-    name: 'Eden Clements',
-    number: '645-17-79',
-  },
-  {
-    id: 'id-4',
-    name: 'Annie Copeland',
-    number: '227-91-26',
-  },
-];
+// [
+//     {
+//         id: 'id-1',
+//         name: 'Rosie Simpson',
+//         number: '459-12-56',
+//     },
+// ];
 
 const slice = createSlice({
   name: 'contacts',
